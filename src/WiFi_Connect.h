1c1840bdd48b836427fe13a1505ffce76871a272
Tesupersnake <supersnakeplay@gmail.com>
2023-01-23 19:57:49 +0300

    k

---
+++ src/WiFi_Connect.h
@@ -0,0 +1,13 @@
+#include <ESP8266WiFi.h>
+int n = 0;
+
+bool connectWifi(char ssid, char password, int timeout){
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+        n++;
+        if(n==timeout) return false;
+    }
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+<<<<<<< Updated upstream
+=======
+#include "WiFi_Connect.h"
+#include "Configuration.h"
+>>>>>>> Stashed changes
 
+bool wifi;
 void setup() {
-  
+  wifi = connectWifi(APPSSID);
 }
 
 void loop() {
